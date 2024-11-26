29dec5c230c17b093201ce6c03ef4be51cb43243
vladyslav-semenets <developerdev95@gmail.com>
2024-11-26 16:57:36 +0100

    refactor app.component

--- client/javascript/src/app/app.component.ts
+++ client/javascript/src/app/app.component.ts
@@ -41,230 +41,353 @@ interface IControllerEvent extends Event {
 		MatProgressSpinner,
 	],
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 	direction: unknown;
-	controller: IControllerEvent['detail']['controller'];
+	controller: unknown = null;
 	turnTo: unknown;
-	isCarStarted = false;
-	degreeOfTurns = 87;
-	pwmFrequency = 25000;
-	carSpeed = 50;
-	isCameraStarted = true;
-	rcCarCameraUrl = '';
+	isCarStarted: boolean = false;
+	degreeOfTurns: number = 87;
+	pwmFrequency: number = 25000;
+	carSpeed: number = 50;
+	isCameraStarted: boolean = true;
+	rcCarCameraUrl: string = '';
 
-	constructor(private appService: AppService) {
-		this.appService.initWebSocket();
+	constructor(
+		private _appService: AppService,
+	) {
+		this._appService.initWebSocket();
 	}
 
-	ngOnInit(): void {
-		this.setupController();
-		this.setupSocketListeners();
-	}
+	public ngOnInit(): void {
+		this.setUpController();
 
-	private setupSocketListeners(): void {
-		this.appService.socket.addEventListener('message', this.handleSocketMessage.bind(this));
-		this.appService.socket.addEventListener('open', this.initializeCar.bind(this));
-	}
+		this._appService.socket.addEventListener(
+			'message',
+			(event) => {
+				try {
+					const payload = JSON.parse(event.data);
 
-	private handleSocketMessage(event: MessageEvent): void {
-		try {
-			const payload = JSON.parse(event.data);
+					this.isCarStarted = !!payload?.carStarted;
 
-			this.isCarStarted = !!payload?.carStarted;
-			if (payload?.cameraURL) {
-				this.rcCarCameraUrl = `rtsp://${payload.cameraURL.replace('tcp://', '')}/cam1`;
-			}
-		} catch (error) {
-			console.error('Error parsing socket message:', error);
-		}
-	}
-
-	private initializeCar(): void {
-		this.appService.socket.send(JSON.stringify({
-			to: 'rc-car-server',
-			data: {
-				action: 'init',
-				carSpeed: this.carSpeed,
-				degreeOfTurns: this.degreeOfTurns,
-			},
-		}));
-	}
-
-	private setupController(): void {
-		(window as any)?.Controller?.search();
-
-		fromEvent<IControllerEvent>(window, 'gc.controller.found')
-			.subscribe(
-				{
-					next: (event) => {
-						this.controller = event.detail.controller;
-						console.log(`Controller found: ${this.controller?.name} (Index: ${this.controller?.index})`);
-					},
-				},
-			);
-
-		this.setupControllerButtons();
-		this.setupAnalogStickEvents();
-	}
-
-	private setupControllerButtons(): void {
-		const buttonActions: Record<string, () => void> = {
-			'FACE_1': () => this.stopCar(),
-			'FACE_2': () => this.startCamera(),
-			'FACE_3': () => this.stopCamera(),
-			'FACE_4': () => this.setEscToNeutralPosition(),
-			'DPAD_RIGHT': () => this.updateDegreeOfTurns(-1),
-			'DPAD_LEFT': () => this.updateDegreeOfTurns(1),
-			'LEFT_SHOULDER': () => this.breakCar(),
-		};
-
-		fromEvent<IControllerEvent>(window, 'gc.button.press')
-			.subscribe(
-				{
-					next: (event) => {
-						const action = buttonActions[event.detail.name];
-						if (action) {
-							action();
-						}
+					if (payload?.cameraURL) {
+						this.rcCarCameraUrl = `rtsp://${payload.cameraURL.replace('tcp://', '')}/cam1`;
 					}
+
+				} catch (e) {
+					console.error(e);
 				}
-			);
-
-		fromEvent<IControllerEvent>(window, 'gc.button.release')
-			.pipe(
-				filter(
-					event => [
-						'RIGHT_SHOULDER_BOTTOM',
-						'LEFT_SHOULDER_BOTTOM',
-					].includes(event.detail.name),
-				),
-			)
-			.subscribe(
-				{
-					next: () => this.setEscToNeutralPosition(),
-				},
-			);
-	}
-
-	private setupAnalogStickEvents(): void {
-		this.configureAnalogStick(
-			'RIGHT_ANALOG_STICK',
-			'camera-gimbal-set-pitch-angle',
-			{
-				y: [0, 90, 0.01],
-				negate: true,
-			},
-		);
-		this.configureAnalogStick(
-			'RIGHT_ANALOG_STICK',
-			'camera-gimbal-set-pitch-angle',
-			{
-				y: [0, -79, 0.01],
-			},
-		);
-		this.configureAnalogStick(
-			'LEFT_ANALOG_STICK',
-			'turn-to',
-			{
-				x: [0, this.degreeOfTurns, 0.01],
-				action: 'right',
-			},
-		);
-		this.configureAnalogStick(
-			'LEFT_ANALOG_STICK',
-			'turn-to',
-			{
-				x: [this.degreeOfTurns, 140, 0.01],
-				action: 'left',
-			},
-		);
-	}
-
-	private configureAnalogStick(
-		stickName: string,
-		action: string,
-		{
-			x,
-			y,
-			negate,
-			action: turnDirection,
-		}: {
-			x?: [number, number, number];
-			y?: [number, number, number];
-			negate?: boolean;
-			action?: string;
-		}
-	): void {
-		fromEvent<IControllerEvent>(window, 'gc.analog.hold')
-			.pipe(
-				filter(event => event.detail.name === stickName && this.shouldTriggerAnalogEvent(event, x, y, negate)),
-				map(event => this.mapStickToAngle(event.detail.position, x, y)),
-				distinctUntilChanged(),
-			)
-			.subscribe((degrees: number) => {
-				this.turnTo = turnDirection || null;
-				this.sendAction(action, { degrees });
 			});
+
+		this._appService.socket.addEventListener(
+			'open',
+			() => {
+				this.sendSocketMessage('init', {
+					carSpeed: this.carSpeed,
+					degreeOfTurns: this.degreeOfTurns,
+				});
+			},
+		);
 	}
 
-	private shouldTriggerAnalogEvent(
-		event: IControllerEvent,
-		x?: [number, number, number],
-		y?: [number, number, number],
-		negate?: boolean,
-	): boolean {
-		if (x && Math.sign(event.detail.position.x) !== -1 !== negate) {
-			return true;
+	public stopCar(): void {
+		this.direction = null;
+		this.sendSocketMessage('stop-car', {});
+	}
+
+	public startCamera(): void {
+		this.sendSocketMessage('start-camera', {});
+	}
+
+	public stopCamera(): void {
+		this.sendSocketMessage('stop-camera', {});
+	}
+
+	public setEscToNeutralPosition(): void {
+		this.direction = null;
+		this.sendSocketMessage('set-esc-to-neutral-position', {});
+	}
+
+	public breakingCar(): void {
+		this.sendSocketMessage('breaking', {});
+	}
+
+	public resetTurns(): void {
+		this.turnTo = null;
+		this.sendSocketMessage('reset-turns', {
+			degreeOfTurns: this.degreeOfTurns,
+		});
+	}
+
+	public resetCameraGimbal(): void {
+		this.sendSocketMessage('reset-turns', {});
+	}
+
+	public onChangeDegreeOfTurns(value: number): void {
+		this.sendSocketMessage('change-degree-of-turns', {
+			degreeOfTurns: value,
+		});
+	}
+
+	private setUpController(): void {
+		// @ts-ignore
+		window.Controller.search();
+
+		fromEvent<IControllerEvent>(window, 'gc.controller.found').subscribe(
+			(event) => {
+				const controller = event.detail.controller;
+				console.log("Controller found at index " + controller?.index + ".");
+				console.log("'" + controller?.name + "' is ready!");
+				this.controller = controller;
+			},
+		);
+
+		fromEvent<IControllerEvent>(window, 'gc.button.press').subscribe(
+			(event: IControllerEvent) => {
+				if (event.detail.name === 'FACE_1') {
+					this.stopCar();
+				}
+
+				if (event.detail.name === 'FACE_2') {
+					this.startCamera();
+				}
+
+				if (event.detail.name === 'FACE_3') {
+					this.stopCamera();
+				}
+
+				if (event.detail.name === 'FACE_4') {
+					this.setEscToNeutralPosition();
+				}
+
+				if (event.detail.name === 'DPAD_RIGHT') {
+					this.degreeOfTurns--;
+					this.onChangeDegreeOfTurns(this.degreeOfTurns);
+				}
+
+				if (event.detail.name === 'DPAD_LEFT') {
+					this.degreeOfTurns++;
+					this.onChangeDegreeOfTurns(this.degreeOfTurns);
+				}
+
+				if (event.detail.name === 'LEFT_SHOULDER') {
+					this.breakingCar();
+				}
+			},
+		);
+
+		fromEvent<IControllerEvent>(window, 'gc.button.release').pipe(
+			filter(
+				(event: IControllerEvent) => [
+					'RIGHT_SHOULDER_BOTTOM',
+					'LEFT_SHOULDER_BOTTOM',
+				].includes(event.detail.name),
+			),
+		).subscribe(() => {
+			this.setEscToNeutralPosition();
+		});
+
+		// turn gimbal top
+		fromEvent<IControllerEvent>(window, 'gc.analog.hold').pipe(
+			filter(
+				(event: IControllerEvent) =>
+					event.detail.name === 'RIGHT_ANALOG_STICK' && event.detail.position.y < 0
+			),
+			map(
+				(event: IControllerEvent) => this.mapStickToAngle(
+					event.detail.position.y,
+					0,
+					90,
+					0.01,
+				)),
+			distinctUntilChanged(),
+		).subscribe((degrees: number) => {
+			this.sendSocketMessage('change-degree-of-turns', {
+				degrees,
+			});
+		});
+
+		// turn gimbal bottom
+		fromEvent<IControllerEvent>(window, 'gc.analog.hold').pipe(
+			filter(
+				(event: IControllerEvent) =>
+					event.detail.name === 'RIGHT_ANALOG_STICK' && Math.sign(event.detail.position.y) !== -1 && event.detail.position.y > 0
+			),
+			map(
+				(event: IControllerEvent) => {
+					let value  = event.detail.position.y;
+
+					if (value < 0.01) {
+						value = 0.01;
+					}
+					if (value > 1) {
+						value = 1;
+					}
+
+					return value * -79;
+				}),
+			distinctUntilChanged(),
+		).subscribe((degrees: number) => {
+			this.sendSocketMessage('camera-gimbal-set-pitch-angle', {
+				degrees,
+			});
+		});
+
+		// turn gimbal to right
+		fromEvent<IControllerEvent>(window, 'gc.analog.hold').pipe(
+			filter(
+				(event: IControllerEvent) =>
+					event.detail.name === 'RIGHT_ANALOG_STICK' && Math.sign(event.detail.position.x) !== -1
+			),
+			map(
+				(event: IControllerEvent) => this.mapStickToAngle(
+					event.detail.position.x,
+					90,
+					0,
+					0.01,
+				)),
+			distinctUntilChanged(),
+		).subscribe((degrees: number) => {
+			this.sendSocketMessage('camera-gimbal-turn-to', {
+				degrees,
+			});
+		});
+
+		// turn gimbal to left
+		fromEvent<IControllerEvent>(window, 'gc.analog.hold').pipe(
+			filter(
+				(event: IControllerEvent) =>
+					event.detail.name === 'RIGHT_ANALOG_STICK' && event.detail.position.x < 0
+			),
+			map(
+				(event: IControllerEvent) => Math.abs(this.mapStickToAngle(
+					event.detail.position.x,
+					0,
+					90,
+					0.01,
+				)) * -1),
+			distinctUntilChanged(),
+		).subscribe((degrees: number) => {
+			this.sendSocketMessage('camera-gimbal-turn-to', {
+				degrees,
+			});
+		});
+
+		// turn to right
+		fromEvent<IControllerEvent>(window, 'gc.analog.hold').pipe(
+			filter(
+				(event: IControllerEvent) =>
+					event.detail.name === 'LEFT_ANALOG_STICK' && Math.sign(event.detail.position.x) !== -1
+			),
+			map(
+				(event: IControllerEvent) => this.mapStickToAngle(
+					event.detail.position.x,
+					0,
+					this.degreeOfTurns,
+					0.01,
+				)),
+			distinctUntilChanged(),
+		).subscribe((degrees: number) => {
+			this.turnTo = 'right';
+			this.sendSocketMessage('turn-to', {
+				degrees,
+			});
+		});
+
+		// turn to left
+		fromEvent<IControllerEvent>(window, 'gc.analog.hold').pipe(
+			filter(
+				(event: IControllerEvent) =>
+					event.detail.name === 'LEFT_ANALOG_STICK' && event.detail.position.x < 0
+			),
+			map(
+				(event: IControllerEvent) => this.mapStickToAngle(
+					event.detail.position.x,
+					this.degreeOfTurns,
+					140,
+					0.01,
+				)
+			),
+			distinctUntilChanged(),
+		).subscribe((degrees: number) => {
+			this.turnTo = 'left';
+			this.sendSocketMessage('turn-to', {
+				degrees,
+			});
+		});
+
+		fromEvent<IControllerEvent>(window, 'gc.analog.end').subscribe(
+			(event: IControllerEvent) => {
+				if (event.detail.name === 'LEFT_ANALOG_STICK') {
+					this.resetTurns();
+				}
+
+			},
+		);
+
+		fromEvent<IControllerEvent>(window, 'gc.analog.end').subscribe(
+			(event: IControllerEvent) => {
+				if (event.detail.name === 'RIGHT_ANALOG_STICK') {
+					this.resetCameraGimbal();
+				}
+			},
+		);
+		fromEvent<IControllerEvent>(window, 'gc.button.hold').pipe(
+			filter((event: IControllerEvent) => event.detail.name === 'RIGHT_SHOULDER_BOTTOM'),
+			map((event: IControllerEvent) => this.buttonValueToCarSpeed(event.detail.value)),
+			distinctUntilChanged(),
+		).subscribe((carSpeed: number) => {
+			this.sendSocketMessage('forward', {
+				pwmFrequency: this.pwmFrequency,
+				carSpeed,
+			});
+		});
+
+		fromEvent<IControllerEvent>(window, 'gc.button.hold').pipe(
+			filter((event: IControllerEvent) => event.detail.name === 'LEFT_SHOULDER_BOTTOM'),
+			map((event: IControllerEvent) => this.buttonValueToCarSpeed(event.detail.value)),
+			distinctUntilChanged(),
+		).subscribe((carSpeed: number) => {
+			this.sendSocketMessage('backward', {
+				pwmFrequency: this.pwmFrequency,
+				carSpeed,
+			});
+		});
+	}
+
+	private buttonValueToCarSpeed(rawValue: number, maxSpeed: number = 100): number {
+		if (rawValue < 0) {
+			rawValue = 0;
 		}
 
-		if (y && Math.sign(event.detail.position.y) !== -1 !== negate) {
-			return true;
-		}
+		let speed = Math.round((rawValue / 0.6) * 100);
 
-		return false;
+		return speed > maxSpeed ? maxSpeed : speed;
 	}
 
 	private mapStickToAngle(
-		position: { x: number; y: number },
-		xParams?: [number, number, number],
-		yParams?: [number, number, number],
+		stickValue: number,
+		angleMin: number,
+		angleMax: number,
+		step: number
 	): number {
-		const [min, max, step] = xParams || yParams || [0, 0, 1];
-		const value = xParams ? position.x : position.y;
-		return Math.round(((value + 1) * (max - min) / 2 + min) / step) * step;
+		if (Math.sign(stickValue) !== -1) {
+			let angle = angleMax - stickValue * (angleMax - angleMin);
+			angle = Math.round(angle / step) * step;
+			return Math.min(Math.max(angle, Math.min(angleMin, angleMax)), Math.max(angleMin, angleMax));
+		}
+
+		const clampedValue = Math.max(Math.min(stickValue, 0), -1);
+		const angle = angleMax + (clampedValue + 1) * (angleMin - angleMax);
+		const roundedAngle = Math.round(angle / step) * step;
+
+		return Math.max(angleMin, Math.min(roundedAngle, angleMax));
 	}
 
-	private sendAction(action: string, data: Record<string, unknown>): void {
-		this.appService.socket.send(JSON.stringify({
-			to: 'rc-car-server',
-			data: { action, ...data },
-		}));
-	}
-
-	private updateDegreeOfTurns(change: number): void {
-		this.degreeOfTurns += change;
-		this.sendAction('change-degree-of-turns', { degreeOfTurns: this.degreeOfTurns });
-	}
-
-	private stopCar(): void {
-		this.sendAction('stop-car', {});
-	}
-
-	private startCamera(): void {
-		this.sendAction('run-mediamtx', {});
-	}
-
-	private stopCamera(): void {
-		this.sendAction('stop-mediamtx', {});
-	}
-
-	private setEscToNeutralPosition(): void {
-		this.sendAction('set-esc-to-neutral-position', {});
-	}
-
-	private breakCar(): void {
-		this.sendAction('breaking', {});
+	private sendSocketMessage(action: string, data: Record<string, any> = {}): void {
+		this._appService.socket.send(
+			JSON.stringify({ to: 'rc-car-server', data: { action, ...data } })
+		);
 	}
 }
