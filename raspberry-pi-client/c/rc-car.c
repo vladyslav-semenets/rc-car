997ec217840e808cba88f1e7f067c25df594835d
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-02-10 09:23:39 +0100

    update enableDisableEsc func

--- raspberry-pi-client/c/rc-car.c
+++ raspberry-pi-client/c/rc-car.c
@@ -172,10 +172,11 @@ void move(const int *speed, const char *direction) {
 }
 
 void setEscToNeutralPosition() { gpioServo(CAR_ESC_PIN, CAR_ESC_NEUTRAL_PWM); }
+
 void enableDisableEsc() {
+  gpioWrite(CAR_ESC_ENABLE_PIN, 1);
+  usleep(5000000);
   gpioWrite(CAR_ESC_ENABLE_PIN, 0);
-//  usleep(5000000);
-//  gpioWrite(CAR_ESC_ENABLE_PIN, 1);
 }
 
 void startCamera() {
