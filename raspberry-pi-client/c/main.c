38adb8fa99e1d0d3c70ec44c769cc6b9d84143a8
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-04-27 10:18:02 +0200

    gps

--- raspberry-pi-client/c/main.c
+++ raspberry-pi-client/c/main.c
@@ -6,11 +6,14 @@
 #include "libs/env/dotenv.h"
 #include "websocket.h"
 #include "rc-car.h"
+#include <gps.h>
 #include <pthread.h>
 
 int isRunning = 1;
 
 RcCar *rcCar = NULL;
+pthread_t sendGpsDataThread;
+
 
 // Signal handling for clean shutdown
 void handleSignal(const int signal) {
@@ -23,9 +26,42 @@ void handleSignal(const int signal) {
             free(rcCar);
             gpioWrite(CAR_ESC_ENABLE_PIN, 1);
             gpioTerminate();
+            pthread_cancel(sendGpsDataThread);
             exit(0);
         default:
             break;
+    }
+}
+
+void *sendGpsData(void *arg) {
+    struct lws *webSocketInstance = (struct lws *)arg;
+
+    struct gps_data_t gps_data;
+
+    if (gps_open("localhost", "2947", &gps_data) == 0) {
+        gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
+
+        while (true) {
+            if (gps_waiting(&gps_data, 5000000)) {
+                if (gps_read(&gps_data, NULL, 0) > 0) {
+                    if ((gps_data.status == STATUS_FIX) &&
+                        (gps_data.fix.mode >= MODE_2D)) {
+
+                        printf("Speed: %.2f m/s\n", gps_data.fix.speed);
+                        printf("Satellites used: %d\n", gps_data.satellites_used);
+                        printf("Latitude: %f\n", gps_data.fix.latitude);
+                        printf("Longitude: %f\n", gps_data.fix.longitude);
+                        } else {
+                            printf("No fix yet...\n");
+                        }
+                }
+            }
+            usleep(500000);
+        }
+        gps_stream(&gps_data, WATCH_DISABLE, NULL);
+        gps_close(&gps_data);
+    } else {
+        printf("Failed to open GPS.\n");
     }
 }
 
@@ -49,6 +85,8 @@ int main() {
 
     struct sigaction sa;
     WebSocketConnection webSocketConnection = connectToWebSocketServer();
+
+    pthread_create(&sendGpsDataThread, NULL, sendGpsData, webSocketConnection.wsi);
 
     sa.sa_handler = handleSignal;
     sa.sa_flags = 0;
