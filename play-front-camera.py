ab3a3fb35d786086e4bc8e7bb9584b46317f095d
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-09-06 18:19:35 +0200

    add some scripts

--- play-front-camera.py
+++ play-front-camera.py
@@ -1,18 +1,22 @@
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GObject
+from gi.repository import Gst, GLib
 
+# Initialize GStreamer
 Gst.init(None)
 
+# Pipeline string (uses ximagesink instead of autovideosink)
 pipeline_str = (
     'udpsrc port=5000 caps="application/x-rtp, media=video, clock-rate=90000, encoding-name=H264, payload=96" ! '
-    'rtph264depay ! avdec_h264 ! videoconvert ! autovideosink'
+    'rtph264depay ! avdec_h264 ! videoconvert ! ximagesink'
 )
 
+# Create the pipeline
 pipeline = Gst.parse_launch(pipeline_str)
 pipeline.set_state(Gst.State.PLAYING)
 
-loop = GObject.MainLoop()
+# Main loop to keep it running
+loop = GLib.MainLoop()
 try:
     print("Receiving video... Press Ctrl+C to stop.")
     loop.run()
