d87d6e39e165ad6f0250ce6fbaad13e2bd204012
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-09-06 18:28:56 +0200

    update cameras script

--- front-camera.py
+++ front-camera.py
@@ -5,7 +5,7 @@ from gi.repository import Gst, GObject
 Gst.init(None)
 
 pipeline_str = (
-    "v4l2src device=/dev/video0 ! "
+    "v4l2src device=/dev/video2 ! "
     "image/jpeg,width=1280,height=720,framerate=30/1 ! "
     "jpegdec ! "
     "videoflip method=clockwise ! "
