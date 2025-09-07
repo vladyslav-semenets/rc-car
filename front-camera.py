56d89383d1fb6e2201c62c4c70da64451f4dbf7a
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-09-07 14:36:11 +0200

    fix camera

--- front-camera.py
+++ front-camera.py
@@ -6,10 +6,11 @@ Gst.init(None)
 
 pipeline_str = (
     "v4l2src device=/dev/video0 ! "
-    "image/jpeg,width=1280,height=720,framerate=30/1 ! "
+    "image/jpeg,width=960,height=540,framerate=30/1 ! "
     "jpegdec ! "
     "videoflip method=clockwise ! "
-    "videoscale ! video/x-raw,width=1280,height=720 ! "  # <-- changed here
+    "videoscale ! video/x-raw,width=960,height=540 ! "
+    "clockoverlay time-format=\"%Y-%m-%d %H:%M:%S\" shaded-background=true font-desc=\"Sans, 24\" ! "
     "x264enc tune=zerolatency bitrate=2000 speed-preset=ultrafast ! "
     "rtph264pay config-interval=1 pt=96 ! "
     "udpsink host=100.78.40.118 port=5000"
@@ -20,6 +21,8 @@ pipeline.set_state(Gst.State.PLAYING)
 
 try:
     loop = GObject.MainLoop()
+    print("Streaming with current time overlay... Ctrl+C to stop.")
     loop.run()
 except KeyboardInterrupt:
+    print("Stopping pipeline...")
     pipeline.set_state(Gst.State.NULL)
