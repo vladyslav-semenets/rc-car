f357cae1dbdc7f3ed93eef62e1f5c439ac5a0af7
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-09-07 14:57:25 +0200

    fix camera

--- front-camera.py
+++ front-camera.py
@@ -1,4 +1,3 @@
-
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
@@ -7,13 +6,13 @@ Gst.init(None)
 
 pipeline_str = (
     "v4l2src device=/dev/video0 ! "
-    "image/jpeg,width=1280,height=720,framerate=30/1 ! "
+    "image/jpeg,width=640,height=360,framerate=15/1 ! "
     "jpegdec ! "
     "videoflip method=clockwise ! "
-    "videoscale ! video/x-raw,width=960,height=540 ! "
-    "x264enc tune=zerolatency bitrate=2000 speed-preset=ultrafast ! "
+    "videoscale ! video/x-raw,width=640,height=360,format=I420 ! "
+    "x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast key-int-max=30 ! "
     "rtph264pay config-interval=1 pt=96 ! "
-    "udpsink host=100.78.40.118 port=5000"
+    "udpsink host=100.78.40.118 port=5000 sync=false"
 )
 
 pipeline = Gst.parse_launch(pipeline_str)
@@ -21,6 +20,8 @@ pipeline.set_state(Gst.State.PLAYING)
 
 try:
     loop = GObject.MainLoop()
+    print("Streaming optimized video... Ctrl+C to stop.")
     loop.run()
 except KeyboardInterrupt:
+    print("Stopping pipeline...")
     pipeline.set_state(Gst.State.NULL)
