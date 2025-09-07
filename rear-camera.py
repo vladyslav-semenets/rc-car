9aa20a4a6d21f54fdbc47c4e87cc89389599f6a9
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-09-07 15:13:17 +0200

    fix camera

--- rear-camera.py
+++ rear-camera.py
@@ -1,18 +1,18 @@
 import subprocess
 
-cmd = [
-    "gst-launch-1.0",
-    "-v",
-    "v4l2src", "device=/dev/video2",
-    "!", "image/jpeg,width=1280,height=720,framerate=15/1",
-    "!", "jpegdec",
-    "!", "videoflip", "method=clockwise",
-    "!", "videoscale",
-    "!", "video/x-raw,width=480,height=270,format=I420",
-    "!", "x264enc", "tune=zerolatency", "bitrate=800", "speed-preset=ultrafast",
-    "!", "rtph264pay", "config-interval=1", "pt=96",
-    "!", "udpsink", "host=100.78.40.118", "port=5001", "sync=false"
-]
+def rear_camera_stream():
+    cmd = [
+        "gst-launch-1.0", "-v",
+        "v4l2src", "device=/dev/video2",
+        "!", "image/jpeg",
+        "!", "jpegdec",
+        "!", "videoconvert",
+        "!", "x264enc", "tune=zerolatency", "bitrate=800", "speed-preset=ultrafast",
+        "!", "rtph264pay", "config-interval=1", "pt=96",
+        "!", "udpsink", "host=100.78.40.118", "port=5001", "sync=false"
+    ]
+    print("Streaming rear camera via CLI... Ctrl+C to stop.")
+    subprocess.run(cmd)
 
-print("Streaming small rear camera via CLI...")
-subprocess.run(cmd)
+if __name__ == "__main__":
+    rear_camera_stream()
