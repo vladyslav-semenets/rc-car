28142d316ae9b7e54f235e79b370fe4399541066
Vladyslav Semenets <vlad@sourcebreaker.com>
2025-09-07 14:59:44 +0200

    fix camera

--- front-camera.py
+++ front-camera.py
@@ -6,10 +6,10 @@ Gst.init(None)
 
 pipeline_str = (
     "v4l2src device=/dev/video0 ! "
-    "image/jpeg,width=640,height=360,framerate=15/1 ! "
+    "image/jpeg,width=640,height=480,framerate=15/1 ! "
     "jpegdec ! "
     "videoflip method=clockwise ! "
-    "videoscale ! video/x-raw,width=640,height=360,format=I420 ! "
+    "videoconvert ! video/x-raw,format=I420 ! "
     "x264enc tune=zerolatency bitrate=1200 speed-preset=ultrafast key-int-max=30 ! "
     "rtph264pay config-interval=1 pt=96 ! "
     "udpsink host=100.78.40.118 port=5000 sync=false"
@@ -20,7 +20,7 @@ pipeline.set_state(Gst.State.PLAYING)
 
 try:
     loop = GObject.MainLoop()
-    print("Streaming optimized video... Ctrl+C to stop.")
+    print("Streaming optimized video over 4G... Ctrl+C to stop.")
     loop.run()
 except KeyboardInterrupt:
     print("Stopping pipeline...")
