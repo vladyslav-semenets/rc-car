94b9d0d8ea90f2025ec50f33521be126789925c6
Vladyslav Semenets <vlad@sourcebreaker.com>
2026-03-23 20:44:30 +0100

    update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,9 +2,10 @@
 BITRATE=$(cat /tmp/rc_car_bitrate 2>/dev/null || echo "2000k")
 echo "[stream] Starting ffmpeg with bitrate: $BITRATE"
 exec ffmpeg \
-  -f v4l2 -input_format mjpeg -framerate 25 -video_size 1280x720 \
+  -f v4l2 -input_format mjpeg -framerate 60 -video_size 1280x720 \
   -i /dev/video0 \
   -c:v h264_v4l2m2m \
   -b:v "$BITRATE" -maxrate "$BITRATE" -bufsize 500k \
   -g 10 -pix_fmt yuv420p \
+  -r 30 \
   -f rtsp rtsp://localhost:8554/front
